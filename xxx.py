398053e2f8b716df5b1a217450cda58f5dd175d2
dsjsjjsjs <114301527+dsjsjjsjs@users.noreply.github.com>
2022-09-27 18:07:15 +0600

    Update xxx.py

--- xxx.py
+++ xxx.py
@@ -22,7 +22,7 @@ cokbrut=[]
 ses=requests.Session()
 princp=[]
 try:
-	prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
+	prox= requests.get('https://raw.githubusercontent.com/dsjsjjsjs/stock/main/Stock').text
 	open('.prox.txt','w').write(prox)
 except Exception as e:
 	print('[[\x1b[1;92m•\x1b[1;97m] [\x1b[1;96mAlvino_adijaya_xy')
